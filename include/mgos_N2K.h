6a316b85214bff4a12e12032123bed2c9c64bf11
Jim Archer <jim@archer.net>
2019-10-28 21:00:14 -0400

    Initial Commit

---
+++ include/mgos_N2K.h
@@ -0,0 +1,16 @@
+#ifndef __LIB_N2K__
+#define __LIB_N2K__
+
+#include "mgos.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool mgos_N2K_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LIB_N2K__
