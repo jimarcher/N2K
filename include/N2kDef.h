05960f9325e197824c31bdc8d1dbbb7b21d6d54e
Jim Archer <jim@archer.net>
2019-11-14 18:40:31 -0500

    Add conditional macro for millis()

--- include/N2kDef.h
+++ include/N2kDef.h
@@ -41,8 +41,11 @@ extern "C" {
 // Application execution delay. Must be implemented by application.
 extern void delay(uint32_t ms);
 
+// JWA Add this macro as a test
+#ifdef NEED_MILLIS
 // Current uptime in milliseconds. Must be implemented by application.
-extern uint32_t millis();
+ extern uint32_t millis();
+#endif
 }
 #endif
 
